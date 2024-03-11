62e8cd9863d975c8088b04af5a862bbe45b2bc7f
mehdi <mehdimoungids@gmail.com>
2024-03-11 15:04:30 +0000

    Initial commit

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -36,7 +36,6 @@ function Dashboard() {
                         {/*  <!-- Sidebar - Brand --> */}
                         <a className="sidebar-brand d-flex align-items-center justify-content-center" href="#">
                             <div className="sidebar-brand-icon rotate-n-15">
-                                <i className="fas fa-laugh-wink"></i>
                             </div>
                             <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
                             <div className="text-center d-none d-md-inline">
